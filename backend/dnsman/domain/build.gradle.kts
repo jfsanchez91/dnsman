7c4ebcaefb7a069ed3ba81e139386c39fc3652e4
Jorge F. Sánchez (George) <jorge.sanchez@syrocon.de>
2023-08-14 22:35:42 +0200

    Add JUnit engine to Gradle domain

---
+++ backend/dnsman/domain/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher") {
+        because("Only needed to run tests in a version of IntelliJ IDEA that bundles older versions")
+    }
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
+}
